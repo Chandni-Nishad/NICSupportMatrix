a8b5f3e2ae10dc2eb4a40de27aa71bae1bb4a526
Chandni-Nishad <chandninishad953@gmail.com>
2024-03-13 15:02:19 +0530

    some changes done

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavbarComponent,HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent,HeaderComponent,SliderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
